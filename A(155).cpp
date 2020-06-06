442cab19f006082db6004186636a59cbfb0fd638
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-06-06 06:41:44 +0600

    Add files via upload

---
+++ A(155).cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a,b,c;
+    set<int>s;
+    cin>>a>>b>>c;
+    s.insert(a);
+    s.insert(b);
+    s.insert(c);
+    if(s.size()==2){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
