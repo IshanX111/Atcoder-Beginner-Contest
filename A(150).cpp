93178fa9f24238482fc87d0f2c83a4a0dc14e558
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-06-06 06:40:15 +0600

    Add files via upload

---
+++ A(150).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a,b;
+    cin>>a>>b;
+    if(a*500>=b){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+
+
+
+
+}
