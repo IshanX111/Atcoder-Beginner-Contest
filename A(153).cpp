d1bc04060d5a91661131fbd5b487bea9eeded67a
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-06-06 06:41:11 +0600

    Add files via upload

---
+++ A(153).cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int h,a;
+    cin>>h>>a;
+    int c=0;
+    while(h>0){
+        h=h-a;
+        c++;
+    }
+    cout<<c<<endl;
+
+
+
+
+}
+
+
+
+
+
+
+
