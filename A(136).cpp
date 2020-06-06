822474d2e7a0ffc5b802985db2f7919c2052e5b7
Tajrian Islam Ishan <51012063+IshanX111@users.noreply.github.com>
2020-06-07 00:01:52 +0600

    Add files via upload

---
+++ A(136).cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a,b,c,res;
+    cin>>a>>b>>c;
+    res=a-b;
+    c=c-res;
+    if(c<0){
+        cout<<"0"<<endl;
+    }
+    else{
+        cout<<c<<endl;
+    }
+
+}
+
+
+
+
+
+
+
